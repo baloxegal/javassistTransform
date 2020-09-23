2c977438f328d45d685038d53b2a30073ec91717
vbalan <vbalan@patriot.ambianta.local>
2020-09-23 17:22:10 +0300

    98741236

--- src/main/Application.java
+++ src/main/Application.java
@@ -11,12 +11,16 @@ public class Application {
 
 	public static void main(String[] args) throws NotFoundException, CannotCompileException, IOException {
 	
+		Box box = new Box();
+		System.err.println(box.toString());
+		
 		ClassTransform.transform();
 		
-		Box box = new Box();
+		Box box_1 = new Box();
 		
 		
-		System.err.println(box.toString());
+		//System.err.println(box.toString());
+		System.err.println(box_1.toString());
 
 	}
 
--- src/transform/ClassTransform.java
+++ src/transform/ClassTransform.java
@@ -14,22 +14,20 @@ public class ClassTransform {
 	public static Class<?> transform () throws NotFoundException, CannotCompileException, IOException{
 		
 		ClassPool pool = ClassPool.getDefault();
-		
-//		pool.appendClassPath("D:\\vbalan_Java_3\\javassistTransform\\bin");
-//		pool.appendClassPath("bin");
-		
+				
 		CtClass cc = pool.get("domain.Box");
 		
-		cc.defrost();
-		
+				
 		CtConstructor ccons = cc.getDeclaredConstructor(null);
-		cc.removeConstructor(ccons);
+		//cc.removeConstructor(ccons);
 		
-		CtConstructor cstructor = CtNewConstructor.defaultConstructor(cc);
+		ccons.setBody("this.id = 40;");
 		
-		cstructor.setBody("this.id = 50;");
-		
-		cc.addConstructor(cstructor);
+//		CtConstructor cstructor = CtNewConstructor.defaultConstructor(cc);
+//		
+//		cstructor.setBody("this.id = 50;");
+//		
+//		cc.addConstructor(cstructor);
 		
 		return cc.toClass();
 		
